c9a621352342836955b58793ece46ccabb70aeb5
zr-silann <zpaulasilann@gmail.com>
2026-09-06 14:11:00 +0800

    Refactor fetchData and animation functions

--- script/main.js
+++ script/main.js
@@ -345,7 +345,7 @@ const stopPopSound = () => {
         repeatDelay: 1.4,
         onStart: playPopSoundInstantly,   // Plays sound when the animation first starts
         onRepeat: playPopSoundInstantly,
-        onComplete: StopPopSound// Plays sound again for every repeat cycle
+        onComplete: stopPopSound// Plays sound again for every repeat cycle
       },
       0.3
     )
